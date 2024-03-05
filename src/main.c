1816e0a2fe01e224fdd1cc04a73e1849b52dd746
KzaaOmegalul <zzcross2002@gmail.com>
2024-03-05 15:32:40 -0700

    Completed Basic UI Elements

--- src/main.c
+++ src/main.c
@@ -5,10 +5,10 @@ int main()
 {
   char userInput[100];
   while(1)
-  {
-    printf(">");
-    scanf("%[^\n]", userInput);
-    printf("%s\n", userInput);
-  }
+    {
+      printf(">");
+      scanf(" %[^\n]", userInput);
+      printf("%s\n", userInput);
+    }
   return 0;
 }
