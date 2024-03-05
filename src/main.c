34181951772ab4e4b8816da487391a260081ba09
KzaaOmegalul <zzcross2002@gmail.com>
2024-03-05 12:09:57 -0700

    Completed Simple UI

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char userInput[100];
+  while(1)
+  {
+    printf(">");
+    scanf("%[^\n]", userInput);
+    printf("%s\n", userInput);
+  }
+  return 0;
+}
