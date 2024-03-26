9b34fef22c4ee712e378183f2fd728f66c5fc6c0
KzaaOmegalul <zzcross2002@gmail.com>
2024-03-26 16:42:34 -0600

    Updated UI

--- src/main.c
+++ src/main.c
@@ -5,15 +5,17 @@
 
 int main()
 {
-  char userInput[100];
   int FLAG = 1;
   List* history = init_history();
-  while(FLAG)
+  char** tokens;
+  int id;
+    while(FLAG)
     {
+      char userInput[1000];
       printf("Enter q: Quit Program\nEnter h: Print History\nEnter g: Get History @ Specified ID\n\n");
       printf("Write a Sentence or Enter Valid Character\n");
       printf(">");
-      scanf(" %[^\n]", userInput);
+      scanf(" %[^\n]", &userInput);
       switch(*userInput)
 	{
 	case 'q':
@@ -23,14 +25,13 @@ int main()
 	  print_history(history);
 	  break;
 	case 'g':
-	  int id;
 	  printf("Enter A Valid History ID:\n");
-	  scanf("%d", id);
+	  scanf("%d", &id);
 	  printf("Word at ID, %d is %s:\n", id, get_history(history, id));
 	  break;
 	default:
-	  char** tokens = tokenize(userInput);
-	  print_tokens(tokens);
+	 tokens = tokenize(userInput);
+	 print_tokens(tokens);
 	}
      }
   return 0;
